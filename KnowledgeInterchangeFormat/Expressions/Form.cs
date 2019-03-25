f8d2cb9de96310f117e730b9902348015a85dc83
John Gietzen <john@gietzen.us>
2019-03-25 13:18:48 -0700

    Fixup sentence.

---
+++ KnowledgeInterchangeFormat/Expressions/Form.cs
@@ -0,0 +1,11 @@
+// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace KnowledgeInterchangeFormat.Expressions
+{
+    /// <summary>
+    /// A <see cref="Sentence"/> or <see cref="Definition"/>.
+    /// </summary>
+    public abstract class Form : Expression
+    {
+    }
+}
