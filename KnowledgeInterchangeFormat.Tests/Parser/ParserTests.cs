66486bdf0ecf300c829a3f4d4091789248b67cca
John Gietzen <john@gietzen.us>
2019-04-30 11:30:20 -0700

    Added tests, fixed issue with parsing explicit relational sentences, improved error messages.

--- KnowledgeInterchangeFormat.Tests/Parser/ParserTests.cs
+++ KnowledgeInterchangeFormat.Tests/Parser/ParserTests.cs
@@ -2,11 +2,26 @@
 
 namespace KnowledgeInterchangeFormat.Tests.Parser
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using KnowledgeInterchangeFormat.Expressions;
     using Xunit;
 
     public class ParserTests
     {
+        public static IEnumerable<object[]> ExplicitOperatorExpressions => new List<object[]>
+        {
+            new object[] { "(holds x)", new ExplicitRelationalSentence(new Constant("X"), Array.Empty<Term>(), null) },
+            new object[] { "(holds ok (value x))", new ExplicitRelationalSentence(new Constant("OK"), new[] { new ExplicitFunctionalTerm(new Constant("X"), Array.Empty<Term>(), null) }, null) },
+        };
+
+        public static IEnumerable<object[]> ExplicitOperatorExpressionsAsImplicit => new List<object[]>
+        {
+            new object[] { "(holds x)", new ImplicitRelationalSentence(new Constant("HOLDS"), new[] { new Constant("X") }, null) },
+            new object[] { "(ok (value x))", new ImplicitRelationalSentence(new Constant("OK"), new[] { new ImplicitFunctionalTerm(new Constant("VALUE"), new[] { new Constant("X") }, null) }, null) },
+        };
+
         public static IEnumerable<object[]> CanonicalExpressions => new List<object[]>
         {
             new object[] { "(role o)\r\n(role x)" },
@@ -20,6 +35,29 @@ namespace KnowledgeInterchangeFormat.Tests.Parser
             var expression = parser.Parse(value);
 
             Assert.Equal(value, expression.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(ExplicitOperatorExpressions))]
+        public void Parse_GivenAnExplicitOperatorExpression_ReturnsTheExpectedExpression(string value, Sentence sentence)
+        {
+            var parser = new KifParser();
+            var expression = parser.Parse(value);
+
+            Assert.Equal(expression.Forms.Single(), sentence);
+        }
+
+        [Theory]
+        [MemberData(nameof(ExplicitOperatorExpressionsAsImplicit))]
+        public void Parse_GivenAnExplicitOperatorExpressionWihtExplicitOperatorsDisabled_ReturnsAnImplicitExpression(string value, Sentence sentence)
+        {
+            var parser = new KifParser(new KifParser.Options
+            {
+                ExplicitOperators = false,
+            });
+            var expression = parser.Parse(value);
+
+            Assert.Equal(expression.Forms.Single(), sentence);
         }
     }
 }
